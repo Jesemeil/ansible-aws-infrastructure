eadcc459ab5c148503ddd7ebe604890c8e4b7b8a
Aghogho <otahaghogho@gmail.com>
2024-11-08 06:48:15 +0100

    first commit

---
+++ src/main/java/com/example/user_registration/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.user_registration.controller;
+
+import com.example.user_registration.model.User;
+import com.example.user_registration.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
